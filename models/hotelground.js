40cf09ce37a74bfa82820dc6acea89048191ce84
Debadyuti <debadyuti.karmakar@gmail.com>
2021-07-29 22:48:49 +0530

    Some basic style added

--- models/hotelground.js
+++ models/hotelground.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const HotelgroundSchema = new Schema({
     title : String,
-    price : String,
+    image : String,
+    price : Number,
     description: String,
     location : String
 });
--- seeds/index.js
+++ seeds/index.js
@@ -29,9 +29,13 @@ const seedDB = async () =>{
     for(let i = 0;i<50;i++){
         
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10 ;
         const hotel = new Hotelground({
             location : `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:`https://source.unsplash.com/collection/483251`,
+            description : 'Deals from your favourite booking sites. All in one place.',
+            price
         })
 
         await hotel.save();
