8811a5707f0040498fadeb84426898e42c133b44
Debadyuti <debadyuti.karmakar@gmail.com>
2021-08-17 21:24:41 +0530

    Defining error template

--- app.js
+++ app.js
@@ -91,8 +91,9 @@ app.all('*', (req,res, next) => {
 
 
 app.use((err,req,res,next) => {
-    const {statusCode = 500, message = 'Something went wrong' } =err;
-    res.status(statusCode).send(message);
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Something went wrong'  ;
+    res.status(statusCode).render('error',{err})
     //res.send("Opss!!Something went wrong");
 })
 
