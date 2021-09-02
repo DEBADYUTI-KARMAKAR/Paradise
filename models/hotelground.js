67710650e60af55d96a2ce3f36d568d071b8d983
Debadyuti Karmakar <debadyuti.karmakar@gmail.com>
2021-09-02 17:25:04 +0530

    Add delete middleware

--- models/hotelground.js
+++ models/hotelground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const HotelgroundSchema = new Schema({
@@ -14,5 +15,15 @@ const HotelgroundSchema = new Schema({
         }
     ]
 });
+
+HotelgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id:{
+                $in: doc.reviews
+            }
+        }) 
+    }
+})
 
 module.exports = mongoose.model('Hotelground', HotelgroundSchema);
