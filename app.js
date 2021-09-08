b7c2f4c521ad4d09f0a2467fb7bd2cbce178757b
Debadyuti Karmakar <debadyuti.karmakar@gmail.com>
2021-09-08 13:41:17 +0530

    Serving Static assets

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const reviews = require('./routes/reviews')
 mongoose.connect('mongodb://localhost:27017/paradise',{
     useNewUrlParser:true,
     useCreateIndex: true,
-    useUnifiedTopology : true
+    useUnifiedTopology : true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -32,7 +33,7 @@ app.set('views', path.join(__dirname,'views'))
 
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 
