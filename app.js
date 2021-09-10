498d30c6af44ba094c4e132b9d2d662d2f133875
Debadyuti Karmakar <debadyuti.karmakar@gmail.com>
2021-09-10 17:38:18 +0530

    configure session

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+
 const { hotelgroundSchema, reviewSchema } = require('./schemas.js');
 
 const ExpressError = require('./utils/ExpressError');
@@ -34,6 +36,18 @@ app.set('views', path.join(__dirname,'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig ={
+    secret : 'this is secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly:true,
+        expires: Date.now() + 1000 * 60 *60 *24 *7,
+        maxAge :1000 * 60 *60 *24 *7
+    }
+}
+app.use(session(sessionConfig))
 
 
 
