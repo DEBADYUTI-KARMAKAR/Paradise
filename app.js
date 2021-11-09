c59c5a7c5f2ef84f489f1038e604c3a9b29f5602
unknown <debadyuti.karmakar@gmail.com>
2021-11-09 19:37:15 +0530

    configure passport

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
-
-
-
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require("passport-local");
+const User = require('./models/user');
 const Hotelground = require('./models/hotelground');
 const Review = require('./models/review');
 
@@ -51,12 +51,25 @@ const sessionConfig ={
 
 app.use(session(sessionConfig))
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 app.use((req,res,next) =>{
     res.locals.success =  req.flash('success');
     res.locals.error = req.flash('error');    
     next();
+})
+
+app.get('/fakeUser', async (req,res) =>{
+    const user = new User({email: 'dk@gmail.com', username:'dk'})
+    const newUser = await User.register(user,'javajs');
+    res.send(newUser);
 })
 
 app.use('/hotelgrounds', hotelgrounds)
