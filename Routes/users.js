afb8c5ce7f3093c213b6b5ab9fa20491b4762fb4
unknown <debadyuti.karmakar@gmail.com>
2021-11-10 18:56:14 +0530

    Register form

--- Routes/users.js
+++ Routes/users.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+ 
 router.get('/register', (req, res)=>{
 
     res.render('users/register');
 
+})
+
+router.post('/register', async (req,res) => {
+    res.send(req.body);
 })
 
 module.exports = router;
