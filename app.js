475af496f0c4b0d3c1b38cefca3e49a9fbe48e22
unknown <debadyuti.karmakar@gmail.com>
2021-11-12 19:10:57 +0530

    login logout user diff look

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req,res,next) =>{
+    res.locals.currentUser = req.user;
     res.locals.success =  req.flash('success');
     res.locals.error = req.flash('error');    
     next();
