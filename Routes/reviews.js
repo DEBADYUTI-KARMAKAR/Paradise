4c6f0847cdbc78537801b4e81471fad48aeab3d7
unknown <debadyuti.karmakar@gmail.com>
2021-11-17 20:45:10 +0530

    Rating delete button authorization by backend

--- Routes/reviews.js
+++ Routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {validateReview, isLoggedIn} = require('../middleware')
+const {validateReview, isLoggedIn , isReviewAuthor} = require('../middleware');
 const Hotelground = require('../models/hotelground');
 const Review = require('../models/review');
 
@@ -22,7 +22,7 @@ router.post('/', isLoggedIn, validateReview ,catchAsync(async (req, res) => {
     res.redirect(`/hotelgrounds/${hotelground._id}`);
 }))
 
-router.delete('/:reviewId' ,isLoggedIn, catchAsync(async(req,res)=>{
+router.delete('/:reviewId' ,isLoggedIn, isReviewAuthor, catchAsync(async(req,res)=>{
     const {id,reviewId} = req.params;
     await Hotelground.findByIdAndUpdate(id, { $pull: {reviews: reviewId}}); // $pull => remove from mongo
     await Review.findByIdAndDelete(reviewId);
--- middleware.js
+++ middleware.js
@@ -2,6 +2,8 @@ const { hotelgroundSchema, reviewSchema } = require('./schemas.js');
 
 const ExpressError = require('./utils/ExpressError');
 const Hotelground = require('./models/hotelground')
+const Review = require('./models/review');
+
 module.exports.isLoggedIn = (req,res, next) =>{
 if(!req.isAuthenticated()){
     req.session.returnTo = req.originalUrl
@@ -28,6 +30,16 @@ module.exports.isAuthor = async(req,res,next) =>{
     if(!hotelground.author.equals(req.user._id)){
         req.flash('error','Not Permitted');
         res.redirect(`/hotelgrounds/${id}`)
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/hotelgrounds/${id}`);
     }
     next();
 }
