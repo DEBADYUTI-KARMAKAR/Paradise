0124ae28529d021a0c1055c12bcaa8ad35f89fe7
unknown <debadyuti.karmakar@gmail.com>
2021-11-22 23:28:19 +0530

    Install multer, and some basic change new.ejs image section

--- Routes/hotelgrounds.js
+++ Routes/hotelgrounds.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const hotelgrounds = require('../controllers/hotelgrounds')
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, validateHotelground, isAuthor} = require('../middleware')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const Hotelground = require('../models/hotelground');
 
+
 router.route('/')
     .get(catchAsync(hotelgrounds.index))
-    .post(isLoggedIn, validateHotelground, catchAsync(hotelgrounds.createHotelground))
+    //.post(isLoggedIn, validateHotelground, catchAsync(hotelgrounds.createHotelground))
+    .post(upload.array('image'), (req, res) => {
+        res.send(req.body, req.files);
+    })
     
 router.get('/new', isLoggedIn, hotelgrounds.randerNewForm)
 router.route('/:id')
