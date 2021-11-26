ca46703af22fddc4006a27f740cec07dfc9995a1
unknown <debadyuti.karmakar@gmail.com>
2021-11-26 12:53:02 +0530

    fixing seeds

--- seeds/index.js
+++ seeds/index.js
@@ -34,9 +34,20 @@ const seedDB = async () =>{
             author: '618bd35fcd02a11ac8bc4111',
             location : `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image:`https://source.unsplash.com/collection/483251`,
             description : 'Deals from your favourite booking sites. All in one place.',
-            price
+            price,
+            image:  [
+                {
+                 
+                  url: 'https://res.cloudinary.com/dwc6z7wkm/image/upload/v1637910920/Paradise/qanaarczdhzygl0meepl.jpg',
+                  filename: 'Paradise/qanaarczdhzygl0meepl'
+                },
+                {
+                  
+                  url: 'https://res.cloudinary.com/dwc6z7wkm/image/upload/v1637910921/Paradise/zmckoq6rp9je6zf58cyy.jpg',
+                  filename: 'Paradise/zmckoq6rp9je6zf58cyy'
+                }
+              ]
         })
 
         await hotel.save();
