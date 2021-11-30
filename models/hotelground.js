fef4b1899645ee252c0f4c42439d729a25da9549
unknown <debadyuti.karmakar@gmail.com>
2021-11-30 17:48:43 +0530

    adding a thumbnail virtual property

--- models/hotelground.js
+++ models/hotelground.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
-const HotelgroundSchema = new Schema({
-    title : String,
-    image : [
-        {
+const ImageSchema = new Schema({
             url: String,
             filename: String
 
-        }
-    ],
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+})
+
+const HotelgroundSchema = new Schema({
+    title : String,
+    image : [ImageSchema],
     price : Number,
     description: String,
     location : String,
