f2679aa15a121ff2e04cf463c0bfaefdabc794b0
unknown <debadyuti.karmakar@gmail.com>
2021-12-03 23:59:09 +0530

    Display exact location

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,11 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: hotelground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(hotelground.geometry.coordinates)
+    .addTo(map)
