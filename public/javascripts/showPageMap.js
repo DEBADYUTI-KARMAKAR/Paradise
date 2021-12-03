8cce7c5770ea6a9b82c2c5a3941924f6d683b91e
unknown <debadyuti.karmakar@gmail.com>
2021-12-03 23:12:55 +0530

    Display map

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9 // starting zoom
+});
