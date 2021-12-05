cbe7b28aad870bb2ee64eed6cb881b6e58df75f7
unknown <debadyuti.karmakar@gmail.com>
2021-12-05 19:46:44 +0530

    Adding earthquake cluster Map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
+    container:'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: hotelground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
