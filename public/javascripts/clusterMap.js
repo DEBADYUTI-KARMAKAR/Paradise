cb934d4c16cb38235bbbd0a1efa5e9e1997ca059
unknown <debadyuti.karmakar@gmail.com>
2021-12-08 23:33:52 +0530

    Remove inline map style

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-container: 'map',
+container: 'cluster-map',
 style: 'mapbox://styles/mapbox/dark-v10',
 center: [-103.5917, 40.6699],
 zoom: 3
