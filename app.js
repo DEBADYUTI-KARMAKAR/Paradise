0f144f60278cee7116ce7ff392cc2c0d28be5857
unknown <debadyuti.karmakar@gmail.com>
2022-06-03 16:29:37 +0530

    Error stack close

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ app.set('views', path.join(__dirname,'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize());
+app.use(mongoSanitize({  
+    replaceWith: '_'
+}));
 
 
 const sessionConfig ={
